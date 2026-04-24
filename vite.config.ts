17ffbfff618327a227b7cb06dacaf572f116feeb
kiathan <me@kiathan.com>
2026-04-24 10:37:21 +0800

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/paynow-qr-generator',
+  base: '/paynow-qr-generator/',
   plugins: [
     vue(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        image: fileURLToPath(new URL('./image.html', import.meta.url)),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
